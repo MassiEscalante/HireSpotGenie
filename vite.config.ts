87c1f9a76af0f194345a283fef710190e7f77674
MassiEscalante <massiel2611@gmail.com>
2024-10-22 19:34:08 -0700

    fixed port in vite config for render

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: '.',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This will bind the server to 0.0.0.0
+    port: 3000         // You can set this to any port you need
+  }
 });
